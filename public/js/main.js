f888b57c7e6ee5409e0089aae95a83c9b30d71a9
DONGHYUN GOH <aldehf420@naver.com>
2019-04-23 22:09:15 +0900

    merge

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 var game = new Phaser.Game(900, 600, Phaser.AUTO, '');
 
+
 game.state.add('mainMenu', mainMenu);
 game.state.add('Game', Game);
+game.state.add('test', Game);
 game.state.add('ending', Ending);
+
 
 game.state.start('mainMenu');
--- public/js/menu.js
+++ public/js/menu.js
@@ -4,12 +4,14 @@ var mainMenu = {
 
     preload : function() {
         game.load.image('startButton', 'img/startbutton.png');
+        game.load.image('ruleButton', 'img/rulebutton.png')
     },
 
     create : function() {
         game.stage.backgroundColor = '#fff';
         game.add.button(350,270,'startButton', this.startGame, this);
         easyStart = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        game.add.button(380,330,'ruleButton', this.goRule, this);
     },
 
     update : function() {
@@ -21,5 +23,8 @@ var mainMenu = {
     startGame : function() {
         this.state.start('Game');
     },
+    goRule : function(){
+        this.state.start('test');
+    }
 
 }
